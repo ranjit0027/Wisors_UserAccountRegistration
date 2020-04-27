9662c25e04eaea8c350ef36c904a2e1a214dd5d8
ranjit0027 <ranjit0027@gmail.com>
2020-04-28 03:06:34 +0530

    set the time zone for the entire project in RegistrationApplication.java
    set the time zone for the entire project in RegistrationApplication.java

--- Wisors_WS/Wisors/src/main/java/com/wisors/registration/RegistrationApplication.java
+++ Wisors_WS/Wisors/src/main/java/com/wisors/registration/RegistrationApplication.java
@@ -1,5 +1,9 @@
 package com.wisors.registration;
 
+import java.util.TimeZone;
+
+import javax.annotation.PostConstruct;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
@@ -7,7 +11,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
-
 
 /**
  * 
@@ -19,14 +22,19 @@ import org.springframework.web.client.RestTemplate;
 @EnableCaching
 @SpringBootApplication
 public class RegistrationApplication {
-	
-	private static final Logger log = LoggerFactory.getLogger(RegistrationApplication.class);
 
+	private static final Logger log = LoggerFactory.getLogger(RegistrationApplication.class);
 
 	@Bean
 	public RestTemplate createRestTemplate() {
 
 		return new RestTemplate();
+	}
+
+	@PostConstruct
+	public void init() {
+		// Setting Spring Boot SetTimeZone
+		TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
 	}
 
 	public static void main(String[] args) {
